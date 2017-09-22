b19c0ee9e46afb93c2ced8e7661912abb97c52fc
Codetony15 <codetony15@gmail.com>
2017-09-22 13:49:22 -0400

    Add files via upload

--- Awakeningofthecouncil.java
+++ Awakeningofthecouncil.java
@@ -33,6 +33,7 @@ public class Awakeningofthecouncil extends JComponent implements MouseListener {
 	 public static int screen = 1; 
 	public static int clicked = 1; 
 	public static int state = 1; 
+	public static int text = 1; 
 	public static boolean clickable = false;
 	public static String nameOfApp = "Awakening of the Resistance";
 	
@@ -107,9 +108,10 @@ public class Awakeningofthecouncil extends JComponent implements MouseListener {
 	public void TheRoom(Graphics g) {
 		g.setColor(Color.white);
 		g.fillRect(20, 30, 300, 400);
-		if (choiceno == 1){
+		
+		
 			g.fillRect(375, 375, 150, 50);
-		}else if (choiceno == 2){
+		 if (choiceno == 2){
 			g.fillRect(375, 375, 150, 50);
 			g.fillRect(575, 375, 150, 50);
 			
@@ -125,26 +127,31 @@ public class Awakeningofthecouncil extends JComponent implements MouseListener {
 			g.fillRect(575, 300, 150, 50);
 		}
 		
+		
 		g.setColor(Color.black);
 		
 		g.drawString("347: Urk...", 25, 50);
 		
+		if (text == 2){
 		
+		g.drawString("347: W-Where am I?", 25, 65);}else if(text == 3){
+			
 		
-		g.drawString("347: W-Where am I?", 25, 65);
 		
 			
 		
-		g.drawString("347 sits up, Noticing the darkness around him.", 25, 80);
-		
-		g.drawString("347: What the h*## happened last night?", 25, 95);
-		g.drawString("347 stares at the only door in the room.", 25, 110);
+		g.drawString("347 sits up, Noticing the darkness around him.", 25, 80);}
+		else if (text == 4){
+		g.drawString("347: What the h*## happened last night?", 25, 95);}
+		else if (text == 5){
+		g.drawString("347 stares at the only door in the room.", 25, 110);}
+		else if (text == 6){
 		g.drawString("347: Only one place to go...", 25, 125);
 	    choiceno = 2;
 	    
 	    	g.drawString("Leave the Room", 380, 395);
 	    	g.drawString("Stay in the room", 580, 395);
-	    	clickable = true; 
+	    	clickable = true; }
 	    
 	    	
 		
@@ -179,12 +186,21 @@ public class Awakeningofthecouncil extends JComponent implements MouseListener {
 			g.fillRect(575, 300, 150, 50);
 		}
 		g.setColor(Color.black);
-		g.drawString("347: Urk...", 25, 50); 
+		g.drawString("347 opens the door, blinding light floods the room", 25, 50); 
+		g.drawString("347 steps into the light ", 25, 70);
+		g.drawString("He steps into a large room, surrounded by windows", 25, 80);
+		g.drawString("347: What the f*$# happened last night?!", 25, 100);
+		g.drawString("???: Good, you're up.", 25, 110);
+		g.drawString("The feminine voice echoed through a dark hallway,", 25, 130);
+		g.drawString("347 turned to face it", 25, 140);
+		g.drawString("???: I was starting to worry, ", 25, 160);
+		g.drawString("I thought I used too many roofies. Glad I didn't!", 25, 170);
 		
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
+		
 		int x = e.getX();
 		int y = e.getY();
 		if (clickable = true){
@@ -193,9 +209,13 @@ public class Awakeningofthecouncil extends JComponent implements MouseListener {
 			repaint();
 		}else if (x < 524 && x >375 && y <423 && y > 374){
 			screen = screen + 2; 
+			repaint(); 
 		}else if (x < 524 && x >375 && y <325 && y > 275){
 			screen = screen + 3; 
-		}}
+			repaint();
+		}}else{
+			text++;
+		}
 		System.out.println(x + " " + y);
 		
 	}
