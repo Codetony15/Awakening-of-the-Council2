4bcba66775d0986467d57ebf68de9ff2b8fe7da5
Codetony15 <codetony15@gmail.com>
2017-09-28 13:51:28 -0400

    Add files via upload

---
+++ Mission1.java
@@ -0,0 +1,92 @@
+
+public class Mission1 {
+
+	public static void main(String[] args) {
+		//place code in. 
+		
+		g.drawString("Calla: Ready for your first mission?", 25, 50); 
+		//place y/n choice here
+		// if yes:
+		g.drawString("Calla: Awesome! Here's the plan.", 25, 80);
+		g.drawString("The target, Training Facility, Dimension D-8715. ", 25, 100);
+		g.drawString("There's a small server farm down there, ", 25, 110);
+		g.drawString("prisoner data. I bet that's where we can locate her.", 25, 130);
+		g.drawString("All you have to do is go in, make big explosions,", 25, 140);
+		g.drawString("leave no survivors, ", 25, 160);
+		g.drawString("and get the data! Piece of cake!", 25, 170);
+		g.drawString("347: Yeah, until I get my ass blown off at the front door.", 25, 170);
+		g.drawString("How many troops are there anyway?", 25, 170);
+		g.drawString("Calla: Just over 6k! They're rookies though, so no sweat.", 25, 170);
+		g.drawString("347: And how am I supposed to kill 6k rookies alone?", 25, 170);
+		g.drawString("Calla: Pfft, easy. Like with all evil lairs, ", 25, 170);
+		g.drawString("there's a big red button in the bad guy's office!", 25, 170);
+		g.drawString("And of course the button blows the place up.", 25, 170);
+		g.drawString("Why? I don't know! It's just how this works. ", 25, 170);
+		g.drawString("Just beat the boss, hit the button, and leave!", 25, 170);
+		g.drawString("347: Fine. Open me a portal", 25, 170);
+		//if no
+		g.drawString("Calla shoots 347", 25, 80);
+		g.drawString("Calla: YOU'RE READY WHEN I SAY YOUR READY.", 25, 100);
+		g.drawString("DON'T YOU DARE EXCERT YOUR CIS-WHITE-MALE PRIVLEDGE ON ME! ", 25, 110);
+		g.drawString("Game over.", 25, 130);
+		
+		
+		Mission1();
+		
+		//Mission 1 text
+		g.drawString("Calla opens a portal", 25, 80);
+		g.drawString("Calla: Get going! Who know's what they're doing to her! ", 25, 100);
+		g.drawString("347: Alright! Alright! ", 25, 110);
+		g.drawString("347 steps through the portal, ", 25, 130);
+		g.drawString("only to hear alarms start going off", 25, 140);
+		g.drawString("Guard: Unknow person on premises! TAKE HIM OUT! ", 25, 160);
+		g.drawString("347 begins being shot at in all directions.", 25, 170);
+		g.drawString("347(Running): Sh*%, Sh*%, Sh*% ", 25, 170);
+		g.drawString("347 gets behind a tower, with a machine gunner on top.", 25, 170);
+		
+		//Climb up or stay?
+		
+		//if climb up
+		g.drawString("347 climbs on top of the tower", 25, 170);
+		g.drawString("Guard: What the?", 25, 170);
+		g.drawString("347: Listen, I don't want to hurt you. ", 25, 170);
+		g.drawString("Guard: BullSh*%!", 25, 170);
+		g.drawString("The Guard pulls out a gun", 25, 170);
+		
+		//convince him to drop the gun? or chuck his ass off the tower? 
+		// if chuck
+		g.drawString("347 throws his ass off the tower.", 25, 170);
+		g.drawString("347: One problem solved, 5999 to go. ", 25, 170);
+		g.drawString("347 looks at the Machine Gun", 25, 170);
+		g.drawString("347: I shouldn't", 25, 80);
+		//Use the Machine Gun? Or climb down?
+		//if use
+		
+		g.drawString("347: Yeah, I shouldn't ", 25, 100);
+		g.drawString("347 begins walking to the ladder", 25, 80);
+		
+		g.drawString("Random troop: Look! He's too scared to fight! ", 25, 110);
+		g.drawString("What a coward!", 25, 130);
+		g.drawString("347 sprints back to the machine gun ", 25, 140);
+		//play it feels good
+		g.drawString("347 begins firing", 25, 160);
+		g.drawString("347: Who's a coward now? Huh? Huh?", 25, 170);
+		g.drawString("347 starts manically laughing. ", 25, 170);
+		g.drawString("347 runs out of targets.", 25, 170);
+		//end it feels good
+		g.drawString("347: Heh heh heh, that's what I thought.", 25, 80);
+		g.drawString("347 climbs down", 25, 100);
+		g.drawString("347: Almost every problem down, 1 to go.", 25, 110);
+		g.drawString("347 walks to the main building ", 25, 130);
+		g.drawString("only to hear alarms start going off", 25, 140);
+		g.drawString("Guard: Unknow person on premises! TAKE HIM OUT! ", 25, 160);
+		g.drawString("347 begins being shot at in all directions.", 25, 170);
+		g.drawString("347(Running): Sh*%, Sh*%, Sh*% ", 25, 170);
+		g.drawString("347 gets behind a tower, with a machine gunner on top.", 25, 170);
+		
+		
+		
+	}
+	
+	
+}
